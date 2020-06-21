8f120ccd50bbfd04573f91fed804a2f82f22e8fd
Smet <dimkazhr@gmail.com>
2020-06-22 01:16:24 +0300

    Rewrite counting in counting sort.

--- sortings/counting_sort.py
+++ sortings/counting_sort.py
@@ -4,10 +4,7 @@ from utils import sorting_test
 class CountingSort:
     @staticmethod
     def sort(arr: list) -> list:
-        counting_arr = [0 for _ in range(0, max(arr) + 1)]
-
-        for num in arr:
-            counting_arr[num] += 1
+        counting_arr = [arr.count(i) for i in range(0, max(arr) + 1)]
 
         offset = 0
         for i, num in enumerate(counting_arr):
