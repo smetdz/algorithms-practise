9c73abb1aeb3eb6f77dcb2f440f2cc6630714a6e
Smet <dimkazhr@gmail.com>
2020-06-22 01:05:30 +0300

    Create counting sort.

---
+++ sortings/counting_sort.py
@@ -0,0 +1,22 @@
+from utils import sorting_test
+
+
+class CountingSort:
+    @staticmethod
+    def sort(arr: list) -> list:
+        counting_arr = [0 for _ in range(min(arr), max(arr) + 2)]
+
+        for num in arr:
+            counting_arr[num] += 1
+
+        offset = 0
+        for i, num in enumerate(counting_arr):
+            arr[offset: offset + num] = [i for _ in range(num)]
+            offset += num
+
+        return arr
+
+
+if __name__ == '__main__':
+    cs = CountingSort()
+    sorting_test(cs.sort)
